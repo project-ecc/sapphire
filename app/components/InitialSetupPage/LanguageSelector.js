7beba2c4b13dc54942aa330a6cc731f27d024e8e
jawetzel <jawetzel615@gmail.com>
2018-04-28 11:53:55 -0500

    Revert "abstracted lang list on initial landuage picker"
    This reverts commit a54136a9ed81bed81c1d987e94ca1a012ac1d703.

--- app/components/InitialSetupPage/LanguageSelector.js
+++ app/components/InitialSetupPage/LanguageSelector.js
@@ -1,86 +1,78 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { TweenMax } from 'gsap';
-import $ from 'jquery';
-
 import { traduction, language } from '../../lang/lang';
 import * as actions from '../../actions';
-
+import {TweenMax} from "gsap";
+import $ from 'jquery';
 const settings = require('electron').remote.require('electron-settings');
 
 class LanguageSelector extends React.Component {
   constructor() {
     super();
     this.onItemClick = this.onItemClick.bind(this);
-
-    this.state = {
-      langs: [
-        ['bg', 'български (Bulgarian)'],
-        ['zh_cn', '简体中文—中国 (Chinese - CN)'],
-        ['zh_hk', '繁體中文-中華人民共和國香港特別行政區 (Chinese - HK)'],
-        ['nl', 'Nederlands (Dutch)'],
-        ['en', 'English'],
-        ['fr', 'Français (French)'],
-        ['de', 'Deutsch (German)'],
-        ['el', 'ελληνικά (Greek)'],
-        ['ko', '한국어(Korean)'],
-        ['pl', 'Polski (Polish)'],
-        ['pt', 'Português (Portuguese)'],
-        ['ru', 'Русский язык (Russian)'],
-        ['sl', 'Slovenčina (Slovenian)'],
-        ['es', 'Español (Spanish)'],
-        ['tr', 'Türkçe (Turkish)'],
-        ['vn', 'Tiếng việt (Vietnamese)']
-      ]
-    };
   }
 
-  handleDropDownClicked() {
+  handleDropDownClicked(){
     $('.dropdownLanguageSelector').attr('tabindex', 1).focus();
     $('.dropdownLanguageSelector').toggleClass('active');
     $('.dropdownLanguageSelector').find('.dropdown-menuLanguageSelector').slideToggle(300);
   }
 
-  handleDrowDownUnfocus() {
+  handleDrowDownUnfocus(){
     $('.dropdownLanguageSelector').removeClass('active');
     $('.dropdownLanguageSelector').find('.dropdown-menuLanguageSelector').slideUp(300);
   }
 
   onItemClick(event) {
-    const lang = event.currentTarget.dataset.id;
+    let lang = event.currentTarget.dataset.id;
     settings.set('settings.lang', lang);
     this.props.setLang();
   }
 
   render() {
-    return (
+     return (
       <div>
-        <div className="contentLanguageSelector">
-          <p className="selectLanguageLanguageSelector" style={{ fontWeight: '300' }}>
-            {this.props.lang.selectLanguage}
-          </p>
-          <div className="dropdownLanguageSelector" onBlur={this.handleDrowDownUnfocus} onClick={this.handleDropDownClicked}>
-            <div className="selectLanguageSelector">
-              <p style={{ fontWeight: 'normal' }}>{language()}</p>
-              <i className="fa fa-chevron-down" />
+         <div className="contentLanguageSelector">
+           <p className="selectLanguageLanguageSelector" style={{fontWeight:"300"}}>
+             {this.props.lang.selectLanguage}
+           </p>
+            <div className="dropdownLanguageSelector" onBlur={this.handleDrowDownUnfocus} onClick={this.handleDropDownClicked}>
+              <div className="selectLanguageSelector">
+                <p style={{fontWeight:"normal"}}>{language()}</p>
+                <i className="fa fa-chevron-down"></i>
+              </div>
+              <input type="hidden" name="gender"></input>
+              <ul style={{fontWeight:"normal"}} className="dropdown-menuLanguageSelector">
+                  <li onClick={this.onItemClick} data-id="bg">български (Bulgarian)</li>
+                  <li onClick={this.onItemClick} data-id="zh_cn">简体中文—中国 (Chinese - CN)</li>
+                  <li onClick={this.onItemClick} data-id="zh_hk">繁體中文-中華人民共和國香港特別行政區 (Chinese - HK)</li>
+                  <li onClick={this.onItemClick} data-id="nl">Nederlands (Dutch)</li>
+                  <li onClick={this.onItemClick} data-id="en">English</li>
+                  <li onClick={this.onItemClick} data-id="fr">Français (French)</li>
+                  <li onClick={this.onItemClick} data-id="de">Deutsch (German)</li>
+                  <li onClick={this.onItemClick} data-id="el">ελληνικά (Greek)</li>
+                  <li onClick={this.onItemClick} data-id="ko">한국어(Korean)</li>
+                  <li onClick={this.onItemClick} data-id="pl">Polski (Polish)</li>
+                  <li onClick={this.onItemClick} data-id="pt">Português (Portuguese)</li>
+                  <li onClick={this.onItemClick} data-id="ru">Русский язык (Russian)</li>
+                  <li onClick={this.onItemClick} data-id="sl">Slovenčina (Slovenian)</li>
+                  <li onClick={this.onItemClick} data-id="es">Español (Spanish)</li>
+                  <li onClick={this.onItemClick} data-id="tr">Türkçe (Turkish)</li>
+                  <li onClick={this.onItemClick} data-id="vn">Tiếng việt (Vietnamese)</li>
+              </ul>
             </div>
-            <input type="hidden" name="gender" />
-            <ul style={{ fontWeight: 'normal' }} className="dropdown-menuLanguageSelector">
-              {this.state.langs.map(lang => {
-                return (<li key={lang[0]} onClick={this.onItemClick} data-id={lang[0]}>{lang[1]}</li>);
-              })}
-            </ul>
-          </div>
-        </div>
+         </div>
       </div>
-    );
-  }
+      );
+    }
+
 }
 
 const mapStateToProps = state => {
-  return {
+  return{
     lang: state.startup.lang
   };
 };
+
 
 export default connect(mapStateToProps, actions)(LanguageSelector);
