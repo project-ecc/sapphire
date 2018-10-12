f0f0d46d61ea7918ca996fbcedd9f3054d11a6fe
NICHOLAS <nicholaszuccarelli@gmail.com>
2018-10-12 20:28:08 +1100

    Created separate goal component for donations

--- app/components/SettingsPage/Donations.js
+++ app/components/SettingsPage/Donations.js
@@ -3,58 +3,25 @@ import ReactTooltip from 'react-tooltip';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import DonateModal from './partials/DonateModal';
+import Goal from './partials/Goal';
 
 class Donations extends Component {
   constructor(props) {
     super(props);
-    this.animateCircle = this.animateCircle.bind(this);
 
     this.state = {
       selectedGoal: null,
       modalToggle: null
     };
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount() {
-    for (const obj in this.props.donationGoals) {
-      const canvas = this.refs[`donation-${this.props.donationGoals[obj].name}`];
-      const ctx = canvas.getContext('2d');
-      this.animateCircle(ctx, 0, canvas.getAttribute('data-progress'));
-    }
-  }
-
-  handleClick (val) {
+  handleClick(val) {
     this.setState({
       selectedGoal: val
     });
     this.modal.getWrappedInstance().toggle();
-  }
-
-  animateCircle(context, currentPct, endPct) {
-    const elDims = 200;
-    const centerPoint = elDims / 2;
-    const radius = centerPoint - 5;
-    const fullCircle = Math.PI * 2;
-    const quarterCircle = fullCircle / 4;
-
-    context.clearRect(0, 0, elDims, elDims);
-    context.lineWidth = 5;
-
-    context.strokeStyle = '#E7E7E7';
-    context.beginPath();
-    context.arc(centerPoint, centerPoint, radius, 0, fullCircle, false);
-    context.stroke();
-
-    context.strokeStyle = '#DB8F27';
-    context.beginPath();
-    context.arc(centerPoint, centerPoint, radius, -quarterCircle, (fullCircle * currentPct) - quarterCircle, false);
-    context.stroke();
-
-    if ((currentPct += 0.01) < endPct) {
-      requestAnimationFrame(() => {
-        this.animateCircle(context, currentPct, endPct);
-      });
-    }
   }
 
   // the key element is required, otherwise we get a weird behavior (caused by react optimizations) switching between panels that have toggles, feel free to remove the key element and give it a go
@@ -62,30 +29,10 @@ class Donations extends Component {
     return (
       <div>
         <p id="walletBackup">{ this.props.lang.donate }</p>
-        <div className="row col-md-12" style={{ overflowY: 'scroll' }}>
+        <div className="row" style={{ overflowY: 'scroll' }}>
           {this.props.donationGoals.map((goal, index) => {
             return (
-              <div key={index} style={{ borderRadius: '4px', boxShadow: '0px 0px 20px -5px white', margin: '10px', padding: '10px' }} className="community-goal col-md-5">
-                <div className="goal-to-go">
-                  {goal.completedAt != null ? 'Completed' : null}
-                  {goal.daysRemaining != null ? `Days Remaining ${goal.daysRemaining}` : null }
-                </div>
-                <p className="text-center">{goal.name}</p>
-                <hr />
-                <div className="goal-details-container" style={{ textAlign: 'center' }}>
-                  <div className="goal-progress">
-                    <canvas ref={`donation-${goal.name}`} className="goal-progress-circle" height="200" width="200" data-progress={goal.currentBTCTotal / goal.goalBTCTotal} />
-                    <div className="goal-desc" style={{ marginTop: '-130px', marginBottom: '100px' }}>
-                      <div className="goal-current-btc-total"><span className="fa fa-btc" style={{ marginRight: '5px' }} />{ goal.currentBTCTotal }</div>
-                      <div className="goal-of">of</div>
-                      <div className="goal-goal-btc-total"><span className="fa fa-btc" style={{ marginRight: '5px' }} />{ goal.goalBTCTotal }</div>
-                    </div>
-                  </div>
-                  {goal.currentBTCTotal >= goal.goalBTCTotal ? <span className="buttonPrimary caps disabled"><img src="/img/ecc_logo_white.svg" />Thanks</span> :
-                  <div className="buttonPrimary caps" onClick={this.handleClick.bind(this, goal)} data-toggle="modal" data-target={`#goal-${goal.id}-detail`}>
-                    <span className="fa fa-gift" /> Donate</div>}
-                </div>
-              </div>
+              <Goal key={index} goal={goal} handleClick={this.handleClick} />
             );
           })
         }
