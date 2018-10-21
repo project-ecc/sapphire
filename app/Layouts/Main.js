f092e6b52e6743cc9f85da788a49512b46e22803
NICHOLAS <nicholaszuccarelli@gmail.com>
2018-10-21 12:27:52 +1100

    Page transitions

--- app/Layouts/Main.js
+++ app/Layouts/Main.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import { renderRoutes } from 'react-router-config';
+import { Switch } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import {connect} from "react-redux";
 import * as actions from '../actions';
 import Sidebar from './../containers/Sidebar';
 
 class Main extends Component {
   render () {
-    const { route } = this.props
+    const { route, location } = this.props
+    const currentKey = location.pathname.split('/')[1] || '/'
+    const timeout = { enter: 150, exit: 250 }
 
     return (
       <div id="page">
         <Sidebar />
         <div className="container-fluid">
-          {renderRoutes(route.routes)}
+          <TransitionGroup component="main">
+            <CSSTransition key={currentKey} timeout={timeout} classNames="pageTransition" appear>
+              <Switch location={location}>
+                {renderRoutes(route.routes)}
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
         </div>
       </div>
     );
