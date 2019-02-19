2a73c49353245266e5f27509a0cdbc3e0a987c66
NICHOLAS <nicholaszuccarelli@gmail.com>
2019-02-19 20:47:51 +1100

    small var fixes

--- app/daemon/Connector.js
+++ app/daemon/Connector.js
@@ -147,7 +147,7 @@ class Connector extends Component {
 
 
   checkIfDaemonIsRunning() {
-    if (this.state.installedVersion !== -1 && !this.downloading) {
+    if (this.state.installedVersion !== -1 && !this.state.downloadingDaemon) {
       const self = this;
       console.log('Checking if daemon is running...');
       find('name', 'eccoind').then((list) => {
@@ -158,7 +158,7 @@ class Connector extends Component {
           console.log('daemon not running');
           this.props.setDaemonRunning(false);
           // if (!self.downloading) { self.startDaemon(); }
-          if (!self.downloading) { event.emit('start'); }
+          if (!self.state.downloadingDaemon) { event.emit('start'); }
         }
       });
     }
@@ -287,7 +287,9 @@ class Connector extends Component {
     const daemonDownloadURL = getDaemonDownloadUrl();
     console.log(daemonDownloadURL);
 
-    this.downloading = true;
+    this.setState({
+      downloadingDaemon: true
+    });
     const self = this;
 
     return new Promise((resolve, reject) => {
