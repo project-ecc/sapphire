54bda2f3db6d81ea4a32f4b018f65f9718c076dc
Dylan Aird <dylan.aird595@gmail.com>
2019-11-20 20:43:53 +1100

    database message

---
+++ app/MessagingProtocol/DatabaseMessage.js
@@ -0,0 +1,9 @@
+// Initializing a class definition
+class DatabaseMessage {
+  constructor(action, model, content) {
+    this.action = action;
+    this.model = model;
+    this.content = content
+  }
+}
+export default DatabaseMessage;
